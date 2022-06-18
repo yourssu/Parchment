7e12aefc9cb00d028dd309497dd9f545ecbf28fe
Yonghyun <lygon55555@gmail.com>
2022-06-18 14:14:08 +0900

    플랫폼 버전 수정

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Parchment",
-    platforms: [.iOS("9.0")],
+    platforms: [.iOS("13.0")],
     products: [
         .library(name: "Parchment", targets: ["Parchment"]),
     ],
